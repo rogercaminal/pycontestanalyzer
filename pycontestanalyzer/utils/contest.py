988d07c2866e1f850c2cc5476bb544ac01ca4293
rogercaminal <roger.caminal@gmail.com>
2018-12-03 22:37:16 +0100

    Modify retrieve_contest_object function

--- pycontestanalyzer/utils/contest.py
+++ pycontestanalyzer/utils/contest.py
@@ -4,13 +4,20 @@ import logging
 logging.basicConfig(format='%(levelname)s: %(asctime)s UTC  -  contest.py : %(message)s', level=logging.DEBUG)
 
 
-def retrieve_contest_object(search_info):
-    contestType = search_info["name"]
+def retrieve_contest_object(search_info, output_folder):
+    contest_type = search_info["name"]
     callsign = search_info["callsign"]
     year = search_info["year"]
     mode = search_info["mode"]
 
-    logname = "ContestAnalyzerOnline/contestAnalyzer/data/%s_%s_%s_%s/log_%s_%s_%s_%s.log" % (contestType, year, mode, callsign, contestType, year, mode, callsign)
+    logname = "{output_folder}/{contest_type}_{year}_{mode}_{callsign}/log_{contest_type}_{year}_{mode}_{callsign}.log"\
+        .format(
+          output_folder=output_folder,
+          contest_type=contest_type,
+          year=year,
+          mode=mode,
+          callsign=callsign
+        )
 
     contest = None
     with open("%s.pickle" % logname.replace(".log", ""), 'rb') as myinput:
